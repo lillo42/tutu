4b2542757659ace0994ea0a7ccd87ee0c41f7f00
Rafael Lillo <rafa_lillo@outlook.com>
2023-03-10 11:55:44 +0000

    Add Github Action (#2)

--- src/Tutu/Unix/Interop/LibC/ulong_t.cs
+++ src/Tutu/Unix/Interop/LibC/ulong_t.cs
@@ -12,9 +12,9 @@ internal struct ulong_t : IEquatable<ulong_t>
 
     public static explicit operator uint(ulong_t arg) => (uint) arg.Value;
 
-    public static explicit operator ulong_t(ulong arg) => new ulong_t(new size_t(arg));
+    public static explicit operator ulong_t(ulong arg) => new(new size_t(arg));
 
-    public static implicit operator ulong_t(uint arg) => new ulong_t((size_t) arg);
+    public static implicit operator ulong_t(uint arg) => new((size_t) arg);
 
     public override string ToString() => this.Value.ToString();
 
@@ -24,33 +24,33 @@ internal struct ulong_t : IEquatable<ulong_t>
 
     public bool Equals(ulong_t v) => this == v;
 
-    public static ulong_t operator +(ulong_t v) => new ulong_t(v.Value);
+    public static ulong_t operator +(ulong_t v) => new(v.Value);
 
-    public static ulong_t operator ~(ulong_t v) => new ulong_t(~v.Value);
+    public static ulong_t operator ~(ulong_t v) => new(~v.Value);
 
-    public static ulong_t operator ++(ulong_t v) => new ulong_t(v.Value + (size_t) 1);
+    public static ulong_t operator ++(ulong_t v) => new(v.Value + (size_t) 1);
 
-    public static ulong_t operator --(ulong_t v) => new ulong_t(v.Value - (size_t) 1);
+    public static ulong_t operator --(ulong_t v) => new(v.Value - (size_t) 1);
 
-    public static ulong_t operator +(ulong_t v1, ulong_t v2) => new ulong_t(v1.Value + v2.Value);
+    public static ulong_t operator +(ulong_t v1, ulong_t v2) => new(v1.Value + v2.Value);
 
-    public static ulong_t operator -(ulong_t v1, ulong_t v2) => new ulong_t(v1.Value - v2.Value);
+    public static ulong_t operator -(ulong_t v1, ulong_t v2) => new(v1.Value - v2.Value);
 
-    public static ulong_t operator *(ulong_t v1, ulong_t v2) => new ulong_t(v1.Value * v2.Value);
+    public static ulong_t operator *(ulong_t v1, ulong_t v2) => new(v1.Value * v2.Value);
 
-    public static ulong_t operator /(ulong_t v1, ulong_t v2) => new ulong_t(v1.Value / v2.Value);
+    public static ulong_t operator /(ulong_t v1, ulong_t v2) => new(v1.Value / v2.Value);
 
-    public static ulong_t operator %(ulong_t v1, ulong_t v2) => new ulong_t(v1.Value % v2.Value);
+    public static ulong_t operator %(ulong_t v1, ulong_t v2) => new(v1.Value % v2.Value);
 
-    public static ulong_t operator &(ulong_t v1, ulong_t v2) => new ulong_t(v1.Value & v2.Value);
+    public static ulong_t operator &(ulong_t v1, ulong_t v2) => new(v1.Value & v2.Value);
 
-    public static ulong_t operator |(ulong_t v1, ulong_t v2) => new ulong_t(v1.Value | v2.Value);
+    public static ulong_t operator |(ulong_t v1, ulong_t v2) => new(v1.Value | v2.Value);
 
-    public static ulong_t operator ^(ulong_t v1, ulong_t v2) => new ulong_t(v1.Value ^ v2.Value);
+    public static ulong_t operator ^(ulong_t v1, ulong_t v2) => new(v1.Value ^ v2.Value);
 
-    public static ulong_t operator <<(ulong_t v, int i) => new ulong_t(v.Value << i);
+    public static ulong_t operator <<(ulong_t v, int i) => new(v.Value << i);
 
-    public static ulong_t operator >>(ulong_t v, int i) => new ulong_t(v.Value >> i);
+    public static ulong_t operator >>(ulong_t v, int i) => new(v.Value >> i);
 
     public static bool operator ==(ulong_t v1, ulong_t v2) => v1.Value == v2.Value;
 
